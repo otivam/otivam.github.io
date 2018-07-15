b577bdc4d974ad233f89d9dffc9f1e500daaa95b
gincho georgiev <otivam_92@abv.bg>
2018-07-15 17:20:23 +0300

    Add files via upload

---
+++ master.js
@@ -0,0 +1,21 @@
+function openPage(pageName, elmnt, color) {
+
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.backgroundColor = "";
+        tablinks[i].style.color = 'white';
+    }
+
+    document.getElementById(pageName).style.display = "block";
+
+    elmnt.style.backgroundColor = color;
+    elmnt.style.color = 'rgb(21, 55, 112)';
+}
+
+document.getElementById("defaultOpen").click();
